2a35283ebe4b69407ca566a99ea8c4e627093ef0
Robert Norton <spiffspuzzum@users.noreply.github.com>
2018-11-18 14:41:44 -0800

    fixed manifest name, and nav text on mobile

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import React from "react";
 
-const Nav = () => 
-    <nav>
-        <div className="nav-wrapper">
-            <span className="site-title">storyunderground.com</span>
-            <span className="sit-subtitle">stories by robert norton</span>
-        </div>
-    </nav>
+const Nav = () => (
+  <nav>
+    <div className="nav-wrapper">
+      <div className="nav-text-wrapper">
+        <span className="site-title">storyunderground.com</span>
+        <span className="site-subtitle">stories by robert norton</span>
+      </div>
+    </div>
+  </nav>
+);
 
 export default Nav;
